8f016b4f7a53fa267351c40b764e0e64c0795238
vanum jeevan jyothi kumar <92968746+jeevankumar111@users.noreply.github.com>
2024-05-16 02:01:44 +0530

    Update 0088-merge-sorted-array.py

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -1,7 +1,7 @@
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         """
-        Do not return anything, modify nums1 in-place instead0.
+        Do not return anything, modify nums1 in-place instead00.
         last index num 1
         """
         last = m+n-1
